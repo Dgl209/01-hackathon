10ac682b704abc21adc9a741360ecf3624198cb5
dgl <mykulych15@gmail.com>
2022-03-11 19:53:37 +0200

    create contextmenu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,27 @@
 import './styles.css'
+import {ContextMenu} from "./menu";
 
+const contextMenu = new ContextMenu('#menu')
+const menu = document.querySelector('#menu')
+let coordinateX
+let coordinateY
+
+document.body.addEventListener('contextmenu', event => {
+    event.preventDefault()
+    contextMenu.open()
+    menu.style.top = `${coordinateY}px`
+    menu.style.left = `${coordinateX}px`
+})
+
+document.body.addEventListener('mousemove', event => {
+    const { x, y } = event
+    coordinateX = x
+    coordinateY = y
+})
+
+menu.addEventListener('click', event => {
+    // if (event.target.dataset.type === 'test') {
+    //     testModule.trigger()
+    // }
+    contextMenu.close()
+})
