30224efbdb2477cd034b6bcfd6fe463784934e7e
dgl <mykulych15@gmail.com>
2022-03-12 10:39:51 +0200

    add module create figure

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import './styles.css'
+import {cleanBody} from "./utils";
 import {ContextMenu} from "./menu";
+import {FiguresModule} from "./modules/figures.module";
 
 const contextMenu = new ContextMenu('#menu')
+const figuresModules = new FiguresModule('figures', 'Create figure')
 const menu = document.querySelector('#menu')
+
 let coordinateX
 let coordinateY
+
+contextMenu.add(figuresModules)
 
 document.body.addEventListener('contextmenu', event => {
     event.preventDefault()
@@ -13,15 +19,17 @@ document.body.addEventListener('contextmenu', event => {
     menu.style.left = `${coordinateX}px`
 })
 
+menu.addEventListener('click', event => {
+    if (event.target.dataset.type === 'figures') {
+        cleanBody()
+        figuresModules.removeListener()
+        figuresModules.trigger()
+    }
+    contextMenu.close()
+})
+
 document.body.addEventListener('mousemove', event => {
     const { x, y } = event
     coordinateX = x
     coordinateY = y
-})
-
-menu.addEventListener('click', event => {
-    // if (event.target.dataset.type === 'test') {
-    //     testModule.trigger()
-    // }
-    contextMenu.close()
 })
